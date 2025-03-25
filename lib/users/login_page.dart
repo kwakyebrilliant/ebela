22177ecbb51572a89e65b8bdbc4c2fcb06cf4210
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-03-25 23:29:03 +0300

    login screen updated

--- lib/users/login_page.dart
+++ lib/users/login_page.dart
@@ -18,8 +18,10 @@ class _LoginPageState extends State<LoginPage> {
       builder: (context, isKeyboardVisible) {
         return Scaffold(
           resizeToAvoidBottomInset: true,
+          // Container for all contents
           body: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            // 5% of screen width
+            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
             height: screenHeight,
             width: screenWidth,
             decoration: BoxDecoration(
@@ -34,6 +36,69 @@ class _LoginPageState extends State<LoginPage> {
                 ],
                 stops: [0.0, 0.3, 0.6, 1.0],
               ),
+            ),
+
+            // Column for both expanded
+            child: Column(
+              children: [
+                Expanded(
+                  // Stack for both search icon and ebela text
+                  child: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: Align(
+                          alignment: Alignment.center,
+                          child: Icon(
+                            Icons.search,
+                            // 40% of screen width
+                            size: screenWidth * 0.4,
+                            color: Colors.white.withValues(alpha: 0.4),
+                          ),
+                        ),
+                      ),
+
+                      // ebela text centered
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Center(
+                            child: Text(
+                              'ebela',
+                              style: TextStyle(
+                                // 25% of screen width
+                                fontSize: screenWidth * 0.25,
+                                color: Colors.white,
+                                fontWeight: FontWeight.w800,
+                                letterSpacing: 0.3,
+                              ),
+                            ),
+                          ),
+
+                          Text(
+                            'ebela',
+                            style: TextStyle(
+                              // 25% of screen width
+                              fontSize: screenWidth * 0.05,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w800,
+                              letterSpacing: 0.3,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    'data',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: screenWidth * 0.05, // 5% of screen width
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         );
