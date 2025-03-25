257cf1d765120597a0fc58656eb4539e4d173865
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-03-25 22:38:37 +0300

    login background updated

--- lib/users/login_page.dart
+++ lib/users/login_page.dart
@@ -22,6 +22,19 @@ class _LoginPageState extends State<LoginPage> {
             padding: const EdgeInsets.symmetric(horizontal: 20.0),
             height: screenHeight,
             width: screenWidth,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xFF1F6B3B),
+                  Color(0xFF323D2C),
+                  Color(0xFF0E1B12),
+                  Color(0xFF0B0B0B),
+                ],
+                stops: [0.0, 0.3, 0.6, 1.0],
+              ),
+            ),
           ),
         );
       },
