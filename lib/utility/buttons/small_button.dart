bd6ba0ca5fa48644a66d3d5d1158e7135a6c700d
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-03-26 21:04:02 +0300

    variables in small button

--- lib/utility/buttons/small_button.dart
+++ lib/utility/buttons/small_button.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class SmallButton extends StatelessWidget {
-  const SmallButton({super.key});
+  const SmallButton({super.key, this.function, this.text, this.color});
+
+  final dynamic text;
+  final dynamic function;
+  final dynamic color;
 
   @override
   Widget build(BuildContext context) {
