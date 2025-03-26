bfca32a954143d7ce73dc4d9f7e29e512fc16e71
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-03-26 21:09:49 +0300

    small button final

--- lib/utility/buttons/small_button.dart
+++ lib/utility/buttons/small_button.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SmallButton extends StatelessWidget {
-  const SmallButton({super.key, this.function, this.text, this.color});
+  const SmallButton({
+    super.key,
+    this.function,
+    this.text,
+    this.color,
+    this.fontSize,
+    this.size,
+  });
 
   final dynamic text;
   final dynamic function;
   final dynamic color;
+  final dynamic fontSize;
+  final dynamic size;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: function,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12.0),
+        child: Container(
+          height: size,
+          width: size,
+          color: color,
+          child: Center(
+            child: Text(
+              text,
+              style: GoogleFonts.rubik(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: color,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
